023c605bb36fad119506474613808dd93fe2a2a5
USER <user@USERs-MacBook-Pro.local>
2024-02-16 22:17:27 +0100

    done

--- 0x05-react_state/task_2/dashboard/src/CourseList/CourseList.js
+++ 0x05-react_state/task_2/dashboard/src/CourseList/CourseList.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { StyleSheet, css } from "aphrodite";
+import "./CourseList.css";
 import CourseListRow from "./CourseListRow";
 import PropTypes from "prop-types";
 import CourseShape from "./CourseShape";
 
 function CourseList({ listCourses }) {
   return (
-    <table id="CourseList" className={css(styles.table)}>
+    <table id="CourseList">
       <thead>
         <CourseListRow textFirstCell="Available courses" isHeader={true} />
         <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
@@ -21,33 +21,6 @@ function CourseList({ listCourses }) {
     </table>
   );
 }
-
-const styles = StyleSheet.create({
-  table: {
-    marginTop: "2em",
-    width: "100%",
-    border: "1px solid #ddd",
-    fontSize: "1.2rem",
-    marginBottom: "15em",
-    marginLeft: "auto",
-    marginRight: "auto",
-  },
-
-  th: {
-    borderBottom: "1px solid #ddd",
-    width: "80%",
-  },
-
-  td: {
-    width: "80%",
-  },
-
-  tr: {
-    "nth-child(2)": {
-      textAlign: "left",
-    },
-  },
-});
 
 CourseList.propTypes = {
   listCourses: PropTypes.arrayOf(CourseShape),
--- 0x05-react_state/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { StyleSheet, css } from "aphrodite";
 import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
+import { StyleSheet, css } from "aphrodite";
 
 class Notifications extends Component {
   constructor(props) {
@@ -23,10 +23,11 @@ class Notifications extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className={css(styles.menuItem)}>
-          <p>Your notifications</p>
-        </div>
-        {this.props.displayDrawer ? (
+        {!this.props.displayDrawer ? (
+          <div className={css(styles.menuItem)} onClick={this.props.handleDisplayDrawer}>
+            <p>Your notifications</p>
+          </div>
+        ) : (
           <div className={css(styles.Notifications)}>
             <button
               style={{
@@ -44,6 +45,7 @@ class Notifications extends Component {
               aria-label="Close"
               onClick={(e) => {
                 console.log("Close button has been clicked");
+                this.props.handleHideDrawer();
               }}
             >
               <img src={closeIcon} alt="close icon" width="10px" />
@@ -56,11 +58,23 @@ class Notifications extends Component {
               })}
             </ul>
           </div>
-        ) : null}
+        )}
       </React.Fragment>
     );
   }
 }
+
+const opacityAnim = {
+  "0%": { opacity: 0.5 },
+  "100%": { opacity: 1 },
+};
+
+const bounceAnim = {
+  "0%": { transform: "translateY(0px)" },
+  "33%": { transform: "translateY(-5px)" },
+  "66%": { transform: "translateY(5px)" },
+  "100%": { transform: "translateY(0px)" },
+};
 
 const styles = StyleSheet.create({
   Notifications: {
@@ -69,6 +83,16 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: "1.8em",
     right: "0",
+    zIndex: "100",
+    "@media (max-width: 900px)": {
+      width: "100%",
+      padding: "0px",
+      fontSize: 20,
+      position: "relative",
+      right: 0,
+      left: 0,
+      border: "none",
+    },
   },
 
   "notification-header": {
@@ -77,18 +101,42 @@ const styles = StyleSheet.create({
   },
 
   menuItem: {
+    position: "relative",
+    zIndex: 100,
     textAlign: "right",
+    ":hover": {
+      cursor: "pointer",
+      animationName: [opacityAnim, bounceAnim],
+      animationDuration: "1s, 0.5s",
+      animationIterationCount: "3",
+    },
+  },
+
+  ul: {
+    "@media (max-width: 900px)": {
+      padding: 0,
+    },
+  },
+  button: {
+    "@media (max-width: 900px)": {
+      position: "relative",
+      float: "right",
+    },
   },
 });
 
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
   listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
   listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
 };
 
 export default Notifications;
