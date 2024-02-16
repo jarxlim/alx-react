35f9b3529ff874ea85cdbdf99387c940e9f7ccac
USER <user@USERs-MacBook-Pro.local>
2024-02-16 20:07:06 +0100

    done

--- 0x05-react_state/task_1/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_1/dashboard/src/Header/Header.js
@@ -1,31 +1,16 @@
 import React from "react";
 import logo from "../assets/holberton-logo.jpg";
-import { StyleSheet, css } from "aphrodite";
+import "./Header.css";
 
 function Header() {
   return (
     <>
-      <div className={css(styles["App-header"])}>
-        <img src={logo} className={css(styles.img)} alt="logo" />
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  "App-header": {
-    fontSize: "1.4rem",
-    color: "#e0354b",
-    display: "flex",
-    alignItems: "center",
-    borderBottom: "3px solid #e0354b",
-  },
-
-  img: {
-    width: "200px",
-    height: "200px",
-  },
-});
 
 export default Header;
